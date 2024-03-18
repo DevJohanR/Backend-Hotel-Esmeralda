6f8856996da06003611e3c9da424fcf7d9915c72
Luis Alberto Camarillo Flores <luiluego@gmail.com>
2024-03-17 18:33:20 -0600

    Adding  node_modules to .gitignore

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const server = require("./src/app");
 const { connect } = require("./src/db");
-const { PORT } = process.env || 3000;
+const { PORT } = process.env || 3001;
 
 connect.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
