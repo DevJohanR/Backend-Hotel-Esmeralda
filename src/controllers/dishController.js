67a901d3d9596e5c4bb101597f149d2fca8c49dc
alfonsovm13 <alfonsovengoechea@gmail.com>
2024-03-19 19:56:09 -0500

    prueba

--- src/controllers/dishController.js
+++ src/controllers/dishController.js
@@ -25,23 +25,31 @@ exports.getDishes = async (req, res) => {
 
 exports.editDishes = async (req, res) => {
   try {
-    const { id } = req.params;
-    const dishToUpdate = await dish.findByPk(id); 
-    if (!dishToUpdate) {
-      return res.status(404).json({ message: "Plato no encontrado." });
-    }
-    const editDish = await dishToUpdate.update(req.body); 
-    const dishUpdated = await dish.findByPk(id); 
-
-    if (editDish) {
-      return res.status(200).json({ ...dishUpdated.toJSON(), message: "Plato actualizado correctamente." }); // Se ha agregado .toJSON() para obtener solo los datos del plato actualizado
-    } else {
-      return res.status(400).json({ message: "No se pudo actualizar el plato." });
-    }
+     const { id } = req.params;
+     const dishToUpdate = await dish.findByPk(id); 
+     if (!dishToUpdate) {
+       return res.status(404).json({ message: "Plato no encontrado." });
+     }
+     const editDish = await dishToUpdate.update(req.body); 
+     const dishUpdated = await dish.findByPk(id); 
+ 
+     if (editDish && dishUpdated) {
+       const allDishes = await dish.findAll(); 
+ 
+       return res.status(200).json({ 
+         editedDish: dishUpdated.toJSON(), 
+         allDishes: allDishes.map(d => d.toJSON()), 
+         message: "Plato actualizado correctamente y lista de todos los platos." 
+       }); 
+     } else {
+       return res.status(400).json({ message: "No se pudo actualizar el plato." });
+     }
   } catch (error) {
-    return res.status(500).json({ message: 'Error al editar el plato', error: error.message });
+     return res.status(500).json({ message: 'Error al editar el plato', error: error.message });
   }
-}
+ }
+ 
+
 exports.deleteDishes = async (req, res) => {
   const { id } = req.params;
   try {
