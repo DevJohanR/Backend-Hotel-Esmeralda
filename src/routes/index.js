e3d2af9d95e647317940a7b42cc94b1fbb75fd18
Anthony Joel <ajds.joel1995@gmail.com>
2024-03-22 12:33:50 -0500

    bajando cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,15 +6,6 @@ const authRoutes = require("./users/users");
 
 const router = Router();
 
-<<<<<<< HEAD
- 
-  router.post('/rooms', roomsController.createRoom);
-  router.get('/list', roomsController.listRooms);  
-  router.get('/rooms/:id', roomsController.getRoomDetails);
-  router.put('/rooms/:id', roomsController.updateRoom);
-  router.delete('/rooms/:id', roomsController.deleteRoom);
-  router.get('/filter', roomsController.filterRooms);
-=======
 router.get("/", (req, res) => {
   res.status(200).json({ message: "Bienvenido" });
 });
@@ -23,6 +14,5 @@ router.use("/api/dishes", dishesRoutes);
 router.use("/api/rooms", roomsRoutes);
 router.use("/api/roomstypes", roomsTypesRoutes);
 router.use("/auth", authRoutes);
->>>>>>> fef2cfc97bb8439f7d73cba41b423f74bab6b376
 
 module.exports = router;
