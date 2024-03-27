61db0d5c66024d0b5a28701b92cb9de4cf714199
alfonsovm13 <alfonsovengoechea@gmail.com>
2024-03-26 21:20:16 -0500

    GETROOMDETAIL BY ID

--- src/controllers/rooms/getRoomDetails.js
+++ src/controllers/rooms/getRoomDetails.js
@@ -1,18 +1,21 @@
-  const { rooms, room_details } = require('../../db');
+const { rooms, room_details, room_types } = require('../../db');
 
-  const getRoomDetails = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const room = await rooms.findByPk(id, {
-        include: [{ model: room_details, as: "room_detail" }],
-      });
-      if (!room) {
-        return res.status(404).send("Room not found");
-      }
-      res.status(200).send(room);
-    } catch (error) {
-      res.status(400).send(error.message);
+const getRoomDetails = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const room = await rooms.findByPk(id, {
+      include: [
+        { model: room_details, as: "room_detail" },
+        { model: room_types, as: "room_type" }
+      ],
+    });
+    if (!room) {
+      return res.status(404).send("Room not found");
     }
-  };
+    res.status(200).send(room);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
 
-  module.exports = { getRoomDetails };
+module.exports = { getRoomDetails };
--- src/controllers/users/register.js
+++ src/controllers/users/register.js
@@ -1,41 +1,42 @@
-const bcrypt = require('bcrypt');
-const { Op } = require('sequelize');
-const { users } = require('../../db'); 
+  const bcrypt = require('bcrypt');
+  const { Op } = require('sequelize');
+  const { users } = require('../../db'); 
 
-const register = async (req, res, next) => {
-  try {
-    const { username, email, password } = req.body;
-    // Verificar si el email o username ya están registrados
-    const userExists = await users.findOne({
-      where: {
-        [Op.or]: [{ email }, { username }]
+  const register = async (req, res, next) => {
+    try {
+      const { username, email, password } = req.body;
+      // Verificar si el email o username ya están registrados
+      const userExists = await users.findOne({
+        where: {
+          [Op.or]: [{ email }, { username }]
+        }
+      });
+
+      if (userExists) {
+        return res.status(409).json({ message: 'El correo electrónico o el nombre de usuario ya están en uso.' });
       }
-    }); 
-    if (userExists) {
-      return res.status(409).json({ message: 'El correo electrónico o el nombre de usuario ya están en uso.' });
+      // Encriptar contraseña
+      const hashedPassword = await bcrypt.hash(password, 10); 
+      // Crear el usuario
+      const newUser = await users.create({
+        username,
+        email,
+        password: hashedPassword,
+      });
+
+      return res.status(201).json({
+        message: 'Usuario registrado con éxito',
+        user: {
+          id: newUser.id,
+          username: newUser.username,
+          email: newUser.email,
+        }
+      });
+    } catch (error) {
+      return next(error);
     }
-    // Encriptar contraseña
-    const hashedPassword = await bcrypt.hash(password, 10); 
-    // Crear el usuario
-    const newUser = await users.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
+  };
 
-    return res.status(201).json({
-      message: 'Usuario registrado con éxito',
-      user: {
-        id: newUser.id,
-        username: newUser.username,
-        email: newUser.email,
-      }
-    });
-  } catch (error) {
-    return next(error);
-  }
-};
-
-module.exports = {
-  register
-};
+  module.exports = {
+    register
+  };
