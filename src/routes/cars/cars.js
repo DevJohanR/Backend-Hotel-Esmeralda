9ef51bf0aa94db581bbbadf160daf5f7d48146ef
Favio10 <favio_olivera@hotmail.com>
2024-04-07 10:37:16 -0300

    edit y delete car controller and routes

--- src/routes/cars/cars.js
+++ src/routes/cars/cars.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
 const { createCar } = require("../../controllers/cars/createCar");
 const { listCar } = require("../../controllers/cars/listCard");
+const { deleteCar } = require("../../controllers/cars/deleteCar");
+const { editCars } = require("../../controllers/cars/editCar");
 
 const router = Router();
 
 router.post("/", createCar);
 router.get("/", listCar);
+router.patch("/:id", editCars);
+router.delete("/:id", deleteCar);
 
 module.exports = router;
