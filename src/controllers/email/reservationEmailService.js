c49cc8944f64d5f14d475f66817c869a8d822bc2
DevJohanR <desarrollador7777@gmail.com>
2024-04-17 04:04:06 -0500

    resetPassword-sendgrid complete - confirmReservartion error: NO AUTORIZADO

--- src/controllers/email/reservationEmailService.js
+++ src/controllers/email/reservationEmailService.js
@@ -6,9 +6,16 @@ const sendReservationConfirmationEmail = async ({ username, email, reservationDe
   const message = {
     to: email,
     from: process.env.EMAIL_FROM,
-    subject: 'Your reservation confirmation',
-    text: `Hello ${username}, your reservation for the following services has been confirmed:\n${reservationDetails}`,
-    html: `<strong>Hello ${username}</strong>,<br>Your reservation for the following services has been confirmed:<br>${reservationDetails}`,
+    subject: 'Your reservation confirmation', // Puedes configurar este valor según necesites
+    templateId: 'd-01be013a3f4446e59a85e5255806951e', // ID de la plantilla de SendGrid
+    dynamicTemplateData: {
+      username: username,
+      reservationNumber: reservationDetails.reservation_number,
+      checkInDate: reservationDetails.check_in_date.toISOString().slice(0, 10), // Formato YYYY-MM-DD
+      checkOutDate: reservationDetails.check_out_date.toISOString().slice(0, 10),
+      totalPaid: reservationDetails.total_price.toFixed(2),
+      reservationDetails: JSON.stringify(reservationDetails) // Aquí puedes personalizar cómo deseas mostrar los detalles
+    },
   };
 
   try {
