801c956d5f4c73ecd3da40ab11017284fb7f8980
DevJohanR <desarrollador7777@gmail.com>
2024-04-18 09:29:09 -0500

    100% SENDGRID FUNCIONAL

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,3 +1,73 @@
+//payment.controller.js
+
+
+
+
+const Stripe = require('stripe');
+const { sendReservationConfirmationEmail } = require('./email/reservationEmailService');
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
+
+const createSession = async (req, res) => {
+  try {
+    const { userId, services, totalPrice, userEmail, userName } = req.body; // Asumimos que userEmail y userName se envían en el body
+    console.log('Total price: ', totalPrice);
+    console.log('Services: ', services);
+    console.log('User ID: ', userId);
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            product_data: {
+              name: 'Services',
+            },
+            currency: 'usd',
+            unit_amount: totalPrice * 100, // Convertimos el precio total a centavos
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3000/bookingFour?payment=success',
+      cancel_url: 'http://localhost:3000/bookingFour?payment=cancel',
+      metadata: {
+        userId: userId,
+        services: JSON.stringify(services),
+      },
+    });
+
+    const reservationDetails = JSON.stringify({ services, totalPrice, reservation_number: session.id }); // Asumiendo que el ID de sesión es el número de reserva
+    await sendReservationConfirmationEmail({ username: userName, email: userEmail, reservationDetails });
+
+    console.log(session.url);
+    return res.json({ url: session.url });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createSession };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const Stripe = require('stripe');
 const { sendReservationConfirmationEmail } = require('./email/reservationEmailService');
 const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
@@ -30,9 +100,10 @@ const createSession = async (req, res) => {
         services: JSON.stringify(services),
       },
     });
-
+*/
+/*
     const reservationDetails = JSON.stringify({ services, totalPrice });
-    await sendReservationConfirmationEmail({ username: 'izan7777', email: 'iohan7777@proton.me', reservationDetails });/* username:username */ /* actualizar para enviar - controlador */
+    await sendReservationConfirmationEmail({ username: 'izan7777', email: 'iohan7777@proton.me', reservationDetails });/* username:username */ /* actualizar para enviar - controlador *//*
 
     console.log(session.url);
     return res.json({ url: session.url });
@@ -42,3 +113,4 @@ const createSession = async (req, res) => {
 };
 
 module.exports = { createSession };
+*/
