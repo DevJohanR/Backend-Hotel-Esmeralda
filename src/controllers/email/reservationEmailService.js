a1a5670fd5901b0b70307f2b5e55c029cd4012ec
DevJohanR <desarrollador7777@gmail.com>
2024-04-18 17:50:25 -0500

    se optimiza la logica para que imprima los datos en la plantilla de sendgrid

--- src/controllers/email/reservationEmailService.js
+++ src/controllers/email/reservationEmailService.js
@@ -1,4 +1,48 @@
 // email/reservationEmailService.js
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendReservationConfirmationEmail = async ({ username, email, reservationDetails }) => {
+  const UrlHotel = `http://localhost:3000/checkin`;
+
+  const reservationNumber = reservationDetails.reservation_number;
+  const checkInDate = reservationDetails.check_in_date.toISOString().slice(0, 10); 
+  const checkOutDate = reservationDetails.check_out_date.toISOString().slice(0, 10); 
+  const totalPaid = reservationDetails.total_price.toFixed(2); 
+  const reservationDetailsString = JSON.stringify(reservationDetails); 
+
+  const message = {
+    to: email,
+    from: process.env.EMAIL_FROM,
+    subject: 'Your reservation confirmation',
+    templateId: 'd-01be013a3f4446e59a85e5255806951e',
+    dynamicTemplateData: {
+      username: username,
+      reservationNumber: reservationNumber,
+      checkInDate: checkInDate,
+      checkOutDate: checkOutDate,
+      totalPaid: totalPaid,
+      reservationDetails: reservationDetailsString,
+      UrlHotel: UrlHotel
+    },
+  };
+
+  try {
+    await sgMail.send(message);
+    console.log('Reservation confirmation email sent successfully.');
+  } catch (error) {
+    console.error('Error sending reservation confirmation email:', error);
+    throw error;
+  }
+};
+
+module.exports = {
+  sendReservationConfirmationEmail,
+};
+
+
+/*
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
@@ -34,3 +78,4 @@ const sendReservationConfirmationEmail = async ({ username, email, reservationDe
 module.exports = {
   sendReservationConfirmationEmail,
 };
+*/
