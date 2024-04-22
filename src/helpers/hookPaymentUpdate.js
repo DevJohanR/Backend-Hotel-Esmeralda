2da38cb4dd0d97f7b4c37862410f5d75beb9ef16
alfonsovm13 <alfonsovengoechea@gmail.com>
2024-04-22 12:23:47 -0500

    depuracion hook pay

--- src/helpers/hookPaymentUpdate.js
+++ src/helpers/hookPaymentUpdate.js
@@ -2,22 +2,31 @@ const { user_reservations } = require('../db');
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 
 const handleStripeWebhook = async (req, res) => {
+  console.log('Webhook received');
+  
   const sig = req.headers['stripe-signature'];
   let event;
 
   try {
+    console.log('Constructing event...');
     event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    console.log('Event constructed:', event);
   } catch (err) {
     console.error(`Webhook Error: ${err.message}`);
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
 
   if (event.type === 'checkout.session.completed') {
+    console.log('Checkout session completed');
+    
     const session = event.data.object;
     try {
-      // Buscar la reserva correspondiente y actualizar su estado
+      console.log('Finding reservation...');
       const reservation = await user_reservations.findOne({ where: { stripe_session_id: session.id } });
+      console.log('Reservation found:', reservation);
+      
       if (reservation) {
+        console.log('Updating reservation status...');
         reservation.status = 'pay';
         await reservation.save();
         console.log('Reservation status updated to pay');
@@ -28,6 +37,7 @@ const handleStripeWebhook = async (req, res) => {
     }
   }
 
+  console.log('Sending response to Stripe');
   // Enviar una respuesta al servidor de Stripe para confirmar la recepción del evento
   res.json({ received: true });
 };
