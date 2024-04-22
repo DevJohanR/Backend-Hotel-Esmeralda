418cebd69aa1018ebba525b855c5c65a7350b389
Luis Alberto Camarillo Flores <luiluego@gmail.com>
2024-04-21 19:09:02 -0600

    Production

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const cors = require("cors");
 
 const app = require("./src/app");
 
+app.get("/socket.io/socket.io.js", (req, res) => {
+  res.sendFile(__dirname + "/node_modules/socket.io/client-dist/socket.io.js");
+});
+
 // Crear un servidor HTTP y pasarlo a Socket.IO
 const httpServer = http.createServer(app);
 
@@ -19,7 +23,7 @@ httpServer.listen(DB_PORT, () => {
   console.log(`Main server running on port ${DB_PORT}`);
 });
 
-app.use(cors());
+// app.use(cors());
 
 // No es necesario crear un servidor HTTP separado para Socket.IO
 // ya que httpServer ya está siendo utilizado para ambos propósitos
