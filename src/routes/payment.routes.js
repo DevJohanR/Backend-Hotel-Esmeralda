681b7731ee33d5fbeb021a579cf962c9185631cb
Luis Alberto Camarillo Flores <luiluego@gmail.com>
2024-04-23 23:20:18 -0600

    Prueba

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,17 +1,16 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
 const { createSession } = require("../controllers/payment.controller");
-const { handleStripeWebhook } = require('../helpers/hookPaymentUpdate');
+const { handleStripeWebhook } = require("../helpers/hookPaymentUpdate");
 
 const router = Router();
 
 router.post("/create-checkout-session", createSession);
 
-router.post("/webhookStripe", handleStripeWebhook); 
-
-
-
-
-
+router.post(
+  "/webhookStripe",
+  express.raw({ type: "application/json" }),
+  handleStripeWebhook
+);
 
 module.exports = router;
