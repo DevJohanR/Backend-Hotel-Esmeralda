c2da342925d7ba5a1ab1e3bbea3da8bbbeb7f9a4
Luis Alberto Camarillo Flores <luiluego@gmail.com>
2024-04-23 23:45:28 -0600

    Prueba 2

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,15 +1,18 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const express = require("express");
 const { createSession } = require("../controllers/payment.controller");
 const { handleStripeWebhook } = require("../helpers/hookPaymentUpdate");
 
-
 const router = Router();
 
-router.post("/create-checkout-session", createSession);
-
-router.post("/webhookStripe", express.raw({type: 'application/json'}),  handleStripeWebhook); 
+router.post(
+  "/webhookStripe",
+  express.raw({ type: "application/json" }),
+  handleStripeWebhook
+);
 
 router.use(express.json({ limit: "20mb" }));
+
+router.post("/create-checkout-session", createSession);
 
 module.exports = router;
