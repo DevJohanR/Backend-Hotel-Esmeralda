65a67a8d1b033e96b22f32f8bd64c0ddb8df0565
Luis Alberto Camarillo Flores <luiluego@gmail.com>
2024-04-25 14:03:12 -0600

    debug

--- src/controllers/dishes/editDishes.js
+++ src/controllers/dishes/editDishes.js
@@ -1,28 +1,34 @@
-const { dish } = require('../../db');
+const { dish } = require("../../db");
 
 const editDishes = async (req, res) => {
   try {
-     const { id } = req.params;
-     const dishToUpdate = await dish.findByPk(id); 
-     if (!dishToUpdate) {
-       return res.status(404).json({ message: "Plato no encontrado." });
-     }
-     const editDish = await dishToUpdate.update(req.body); 
-     const dishUpdated = await dish.findByPk(id); 
+    const { id } = req.params;
+    const dishToUpdate = await dish.findByPk(id);
+    if (!dishToUpdate) {
+      return res.status(404).json({ message: "Plato no encontrado." });
+    }
+    console.log(id, req.body);
 
-     if (editDish && dishUpdated) {
-       const allDishes = await dish.findAll(); 
+    const editDish = await dishToUpdate.update(req.body);
+    const dishUpdated = await dish.findByPk(id);
 
-       return res.status(200).json({ 
-         editedDish: dishUpdated.toJSON(), 
-         allDishes: allDishes.map(d => d.toJSON()), 
-         message: "Plato actualizado correctamente y lista de todos los platos." 
-       }); 
-     } else {
-       return res.status(400).json({ message: "No se pudo actualizar el plato." });
-     }
+    if (editDish && dishUpdated) {
+      const allDishes = await dish.findAll();
+
+      return res.status(200).json({
+        editedDish: dishUpdated.toJSON(),
+        allDishes: allDishes.map((d) => d.toJSON()),
+        message: "Plato actualizado correctamente y lista de todos los platos.",
+      });
+    } else {
+      return res
+        .status(400)
+        .json({ message: "No se pudo actualizar el plato." });
+    }
   } catch (error) {
-     return res.status(500).json({ message: 'Error al editar el plato', error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Error al editar el plato", error: error.message });
   }
 };
 
